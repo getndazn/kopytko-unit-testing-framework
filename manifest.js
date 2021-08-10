fc0362ebf0d2f612787ae4243a4f737c385f4215
tomek-r <tomasz.rejment@dazn.com>
2021-08-10 16:26:29 +0200

    feat: Base manifest is added (#13)
    * feat: Manifest is added
    
    * Use provided base manifest

---
+++ manifest.js
@@ -0,0 +1,7 @@
+const baseManifest = require('@kopytko/packager/base-manifest');
+
+module.exports = {
+  ...baseManifest,
+  title: 'Kopytko Unit Testing Framework App',
+  ui_resolutions: 'fhd',
+}
