83f1fefad38cb704bca5a339a599fdcd30a4154f
RadoslawZambrowski <37582383+RadoslawZambrowski@users.noreply.github.com>
2022-05-20 10:09:08 +0200

    fix: Increased default test runner verbosity level (#28)

--- manifest.js
+++ manifest.js
@@ -2,6 +2,7 @@ const baseManifest = require('@dazn/kopytko-packager/base-manifest');
 
 module.exports = {
   ...baseManifest,
+  testRunnerVerbosity: 2,
   title: 'Kopytko Unit Testing Framework App',
   ui_resolutions: 'fhd',
 }
