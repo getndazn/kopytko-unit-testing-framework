46fcda64d1c19867692a1e9afe272f9437cbaef1
Błażej Chełkowski <56542619+bchelkowski@users.noreply.github.com>
2022-08-05 10:02:41 +0200

    fix: add missing bs_const to manifest (#37)

--- manifest.js
+++ manifest.js
@@ -2,6 +2,9 @@ const baseManifest = require('@dazn/kopytko-packager/base-manifest');
 
 module.exports = {
   ...baseManifest,
+  bs_const: {
+    insertKopytkoUnitTestSuiteArgument: true,
+  },
   testRunnerVerbosity: 2,
   title: 'Kopytko Unit Testing Framework App',
   ui_resolutions: 'fhd',
