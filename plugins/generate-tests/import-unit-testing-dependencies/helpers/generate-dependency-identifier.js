b9c8bfbfb368a349adce02ba25ac2eaca37c48f5
Paweł Hertman <pawel@hertman.eu>
2023-07-13 14:06:05 +0200

    fix: Fixed mock identifiers collisions

--- plugins/generate-tests/import-unit-testing-dependencies/helpers/generate-dependency-identifier.js
+++ plugins/generate-tests/import-unit-testing-dependencies/helpers/generate-dependency-identifier.js
@@ -1,7 +1,12 @@
+const getModulePrefixByFilePath = require('@dazn/kopytko-packager/src/plugin-helpers/module/get-module-prefix-by-file-path');
+
 const FILE_NAME_REGEX = /[^/]+.brs/;
 
 module.exports = function generateDependencyIdentifier(dependency) {
-  return dependency.match(FILE_NAME_REGEX)[0]
+  const fileIdentifier = dependency.match(FILE_NAME_REGEX)[0]
     .replace(/[.]|mock|brs/g, '')
     .toLowerCase();
+  const modulePrefix = getModulePrefixByFilePath(dependency);
+
+  return modulePrefix ? `${modulePrefix}/${fileIdentifier}` : fileIdentifier;
 }
